0f581134294bf8f7f9336b5c156453bdfb2c823c
knabrotz <keannanabrotzky@gmail.com>
2022-11-22 01:17:41 -0700

    fix problem in index js

--- pages/index.js
+++ pages/index.js
@@ -22,9 +22,6 @@ export default function Home() {
 
   return (
     <>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
         <h3>Movies</h3>
         {movies?.length > 0 
           ? (
