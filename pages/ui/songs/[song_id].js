8f46ce5b4a8cb805c86466efdcf91ebd37c3bb2f
tklrock <83993369+tklrock@users.noreply.github.com>
2022-12-04 17:02:39 -0700

    Movies, songs, stories lists and individual pages done

--- pages/ui/songs/[song_id].js
+++ pages/ui/songs/[song_id].js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { useRouter } from 'next/router'
 import axios from 'axios';
 import path from 'path';
+import { Progress } from "reactstrap";
 
 
 const Song = () => {
@@ -24,37 +25,45 @@ const Song = () => {
         {song
             ? (
                 <div className="container">
-                    <div className="row">
-                        <div className="col-6">
+                    <div className="row text-center">
+                        <div className="col">
                             <h1>{song.title}</h1>
                         </div>
-                        <div className="col-6">
-                            <h2>{song.artist}</h2>
+                    </div>
+                    <div className="row">
+                        <div className="col-1"></div>
+                        <div className="col-5">
+                            <div className="row">
+                                <h2>{song.artist}</h2>
+                            </div>
+                            <div className="row">
+                                <p style={{fontSize: '15pt'}}>&quot;{song.album}&quot;</p>
+                            </div>
+                            <div className="row">
+                                <div className="col">
+                                <p style={{fontSize: '15pt'}}>{song.year}</p>
+                                </div>                 
+                            </div>
+                            <div className="row">
+                                <div className="col">
+                                <p style={{fontSize: '15pt'}}>{song.Duration}</p>
+                                </div>                 
+                            </div>
+                            <div className="row">
+                                <div className="col text-center">
+                                <a href={song.link} className="spotify" target="_blank">
+                                    <img className="spotifyImg w-50" src="https://pnggrid.com/wp-content/uploads/2021/05/Spotify-PNG-Logo.png"  />
+                                </a>
+                                </div>
+                            </div>
                         </div>
+                        <div className="col-5 text-center">
+                            <img src={song.image} className="w-100 rounded-4"/>
+                        </div>
+                        <div className="col-1"></div>
+                        
                     </div>
                     
-                    <table>
-                        <thead>
-                            <th>ID</th>
-                            <th>Title</th>
-                            <th>Artist</th>
-                            <th>Album</th>
-                            <th>Rating</th>
-                            <th>Year</th>
-                            <th>Description</th>
-                        </thead>
-                        <tbody>
-                            <tr key={song.song_id}>
-                                <td>{song.song_id}</td>
-                                <td>{song.title}</td>
-                                <td>{song.artist}</td>
-                                <td>{song.album}</td>
-                                <td>{song.rating}</td>
-                                <td>{song.year}</td>
-                                <td>{song.description}</td>
-                            </tr>
-                        </tbody>
-                    </table>
                 </div>
             ): <>
                 <div class="d-flex justify-content-center">
